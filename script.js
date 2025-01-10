f698c73b782eff93fd07e37e43cacd1098ef0d21
bke82024 <bke82024@gmail.com>
2025-01-10 13:40:56 +0100

    collision kind of works

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const objWidth = square.offsetWidth;
 const objHeight = square.offsetHeight;
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
+const mousePos = window.mousePos;
 
 let x = 0;
 let y = 0;
@@ -10,24 +11,29 @@ let y = 0;
 window.addEventListener("keydown", (input) => {
   switch (input.key) {
     case "ArrowDown":
-      square.style.transform = changePosition(x, (y += 10));
+      if (x + 10 <= screenWidth)
+        square.style.transform = changePosition(x, (y += 10));
       break;
 
     case "ArrowRight":
-      square.style.transform = changePosition((x += 10), y);
+      if (x + 10 <= screenWidth)
+        square.style.transform = changePosition((x += 10), y);
+
       break;
     case "ArrowLeft":
-      square.style.transform = changePosition((x -= 10), y);
+      if (x - 10 >= -10) square.style.transform = changePosition((x -= 10), y);
       break;
 
     case "ArrowUp":
-      square.style.transform = changePosition(x, (y -= 10));
+      if (y - 10 >= -10) square.style.transform = changePosition(x, (y -= 10));
       break;
 
     default:
       break;
   }
 });
+
+window.addEventListener("click", (click) => {});
 
 function changePosition(x, y) {
   return `translate(${x}px, ${y}px)`;
