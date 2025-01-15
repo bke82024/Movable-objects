dfcd6fbb4ed05d1df0e7663b5e4612805f9e6267
bke82024 <bke82024@gmail.com>
2025-01-15 13:41:12 +0100

    click and arrow keys work

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-const square = document.getElementById("square");
+const movable = document.getElementById("square");
 const canvas = document.getElementById("canvas");
 const moveAmount = 20;
-let x = 0;
-let y = 0;
-
+const move = () => {
+  return { x: 0, y: 0 };
+};
+const pos = move();
 window.addEventListener("keydown", (event) => {
   if (event.key.startsWith("Arrow")) {
-  }
-  event.preventDefault();
+    event.preventDefault();
 
-  switch (event.key) {
-    case "ArrowUp":
-      y -= moveAmount;
-      break;
-    case "ArrowDown":
-      y += moveAmount;
-      break;
-    case "ArrowLeft":
-      x -= moveAmount;
-      break;
-    case "ArrowRight":
-      x += moveAmount;
-      break;
-  }
+    switch (event.key) {
+      case "ArrowUp":
+        pos.y -= moveAmount;
+        move();
+        break;
+      case "ArrowDown":
+        pos.y += moveAmount;
+        break;
+      case "ArrowLeft":
+        pos.x -= moveAmount;
+        break;
+      case "ArrowRight":
+        pos.x += moveAmount;
+        break;
+    }
 
-  square.style.top = `${y}px`;
-  square.style.left = `${x}px`;
+    movable.style.top = `${pos.y}px`;
+    movable.style.left = `${pos.x}px`;
+  }
 });
-const movable = document.getElementById("square");
 
 window.addEventListener("click", (event) => {
   const x = event.clientX - movable.offsetWidth / 2;
   const y = event.clientY - movable.offsetHeight / 2;
+  pos.x = x;
+  pos.y = y;
   movable.style.left = `${x}px`;
   movable.style.top = `${y}px`;
 });
