0203544eeada0f1fe52d91b408c4a330b3e12664
bke82024 <bke82024@gmail.com>
2025-01-15 12:27:10 +0100

    square moves on click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const square = document.getElementById("square");
-const moveAmount = 10;
+const canvas = document.getElementById("canvas");
+const moveAmount = 20;
 let x = 0;
 let y = 0;
 
@@ -7,6 +8,7 @@ window.addEventListener("keydown", (event) => {
   if (event.key.startsWith("Arrow")) {
   }
   event.preventDefault();
+
   switch (event.key) {
     case "ArrowUp":
       y -= moveAmount;
@@ -24,4 +26,12 @@ window.addEventListener("keydown", (event) => {
 
   square.style.top = `${y}px`;
   square.style.left = `${x}px`;
+});
+const movable = document.getElementById("square");
+
+window.addEventListener("click", (event) => {
+  const x = event.clientX - movable.offsetWidth / 2;
+  const y = event.clientY - movable.offsetHeight / 2;
+  movable.style.left = `${x}px`;
+  movable.style.top = `${y}px`;
 });
